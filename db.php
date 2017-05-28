b5081ed9487dba1e105e1a163d516cd8814460e3
Harsh11 <harsh.h.chauhan@gmail.com>
2017-05-28 22:29:41 +0530

    First Commit

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+/* Database connection settings */
+$host = 'localhost';
+$user = 'admine8NgaEg';
+$pass = 'P5vVcfKb4Ixz';
+$db = 'minthings';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
